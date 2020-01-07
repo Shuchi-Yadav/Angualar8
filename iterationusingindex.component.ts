79631fc871af364c8c5c07d4cb54497be781e9a1
Shuchi-Yadav <58326679+Shuchi-Yadav@users.noreply.github.com>
2020-01-07 09:13:20 +0530

    Add files via upload

---
+++ iterationusingindex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iterationusingindex',
+  templateUrl: './iterationusingindex.component.html',
+  styleUrls: ['./iterationusingindex.component.css']
+})
+export class IterationusingindexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
